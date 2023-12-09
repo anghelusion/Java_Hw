d7a28fe978df199d0d3babab60cc1d3222d79258
anghelusion <148393410+anghelusion@users.noreply.github.com>
2023-12-09 19:01:41 +0100

    Add files via upload

--- "lesson3/Main.java
+++ "lesson3/Main.java
@@ -1,33 +1,21 @@
 public class Main {
+
     public static void main(String[] args) {
+        String str = new String("I study Basic Java!");
 
-    int a = 50;
-    int b = 10;
+        printChar(str);
 
-        System.out.println("Сумма: " + sum(a, b));
-        System.out.println("Разность: " + sub(a, b));
-        System.out.println("умножение: " + mul(a, b));
-        System.out.println("Деление: " + div(a, b));
+        String subStr = str.substring(14, 18);
+        System.out.println(subStr);
+
+        String upperStr = str.toUpperCase();
+        System.out.println(upperStr);
+
+        String lowerStr = str.toLowerCase();
+        System.out.println(lowerStr);
     }
 
-
-    public static int sum(int a, int b) {
-        return a + b;
+    public static void printChar(String str) {
+        System.out.println(str.charAt(str.length() - 2));
     }
-
-    public static int sub(int a, int b) {
-        return a - b;
-    }
-
-    public static int mul(int a, int b) {
-        return a * b;
-    }
-
-    public static int div(int a, int b) {
-        return a / b;
-    }
-
-          }
-
-
-
+}
