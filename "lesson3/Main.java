7dba194a3d24bd60f092ed25495880016e5ef4f8
anghelusion <148393410+anghelusion@users.noreply.github.com>
2023-12-09 19:14:18 +0100

    Add files via upload

--- "lesson3/Main.java
+++ "lesson3/Main.java
@@ -27,7 +27,31 @@ public class Main {
         return a / b;
     }
 
-          }
+
+
+
+    String str = new String("I study Basic Java!");
+    public static void printChar(String str) {
+        System.out.println(str.charAt(str.length() - 2));
+
+
+
+        printChar(str);
+
+        String subStr = str.substring(14, 18);
+        System.out.println(subStr);
+
+        String upperStr = str.toUpperCase();
+        System.out.println(upperStr);
+
+        String lowerStr = str.toLowerCase();
+        System.out.println(lowerStr);
+
+    }
+
+    }
+
+
 
 
 
