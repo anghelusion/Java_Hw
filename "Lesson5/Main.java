66cb4daa330c395630bb76ca3509e80f50f14e9f
anghelusion <148393410+anghelusion@users.noreply.github.com>
2023-12-13 15:08:15 +0100

    Add files via upload

---
+++ "Lesson5/Main.java
@@ -0,0 +1,55 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Введите первое число:");
+        double m = scanner.nextDouble();
+
+        System.out.println("Введите второе число:");
+        double n = scanner.nextDouble();
+
+        double mDiff = Math.abs(m - 10);
+        double nDiff = Math.abs(n - 10);
+
+        if (mDiff < nDiff) {
+            System.out.println("Число " + m + " ближе к 10.");
+        } else {
+            System.out.println("Число " + n + " ближе к 10.");
+        }
+    }
+}
+
+//import java.util.Random;
+//
+//public class Main {
+//
+//    public static void main(String[] args) {
+//        Random random = new Random();
+//
+//        int n = random.nextInt(28800 + 1);
+//
+//        System.out.println("Случайное число: " + n);
+//
+//        int hours = n / 3600;
+
+//        switch (hours) {
+//            case 0:
+//                System.out.println("Рабочий день закончился.");
+//                break;
+//            case 1:
+//                System.out.println("Остался 1 час.");
+//                break;
+//            case 2:
+//            case 3:
+//            case 4:
+//                System.out.println("Осталось " + hours + " часа.");
+//                break;
+//            default:
+//                System.out.println("Осталось более " + hours + " часов.");
+//                break;
+//        }
+//    }
+//}
